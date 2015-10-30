7dc8b480421e735125b98a8711a0366631f0426d
Roman Bondarchuk <rmnbnd@gmail.com>
2015-10-30 22:42:30 +0200

    Add utils methods for lexer

---
+++ src/main/java/jsonnet/util/LexerUtils.java
@@ -0,0 +1,37 @@
+package java.jsonnet.util;
+
+public final class LexerUtils {
+
+    public static boolean isUpper(char c) {
+        return c >= 'A' && c <= 'Z';
+    }
+
+    public static boolean isLower(char c) {
+        return c >= 'a' && c <= 'z';
+    }
+
+    public static boolean isNumber(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    public static boolean isSymbol(char c) {
+        switch (c) {
+            case '&':
+            case '|':
+            case '^':
+            case '=':
+            case '<':
+            case '>':
+            case '*':
+            case '/':
+            case '%':
+            case '#':
+                return true;
+        }
+        return false;
+    }
+
+    private LexerUtils() {
+    }
+
+}
