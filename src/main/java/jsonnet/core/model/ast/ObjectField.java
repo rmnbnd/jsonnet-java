682b8664ba3b046bfc1702cadd57bf2babdaaa2f
Roman Bondarchuk <rmnbnd@gmail.com>
2016-03-26 19:09:05 +0200

    [F] - Add kind, id and body for object field

--- src/main/java/jsonnet/core/model/ast/ObjectField.java
+++ src/main/java/jsonnet/core/model/ast/ObjectField.java
@@ -2,4 +2,33 @@ package jsonnet.core.model.ast;
 
 public class ObjectField {
 
+    private Kind kind;
+    private Identifier id;
+    private AST body;
+
+    enum Kind {
+        LOCAL
+    }
+
+    public ObjectField(Kind kind, Identifier identifier, AST body) {
+        this.kind = kind;
+        this.id = identifier;
+        this.body = body;
+    }
+
+    public static ObjectField getLocalField(Identifier identifier, AST body) {
+        return new ObjectField(Kind.LOCAL, identifier, body);
+    }
+
+    public Kind getKind() {
+        return kind;
+    }
+
+    public Identifier getId() {
+        return id;
+    }
+
+    public AST getBody() {
+        return body;
+    }
 }
