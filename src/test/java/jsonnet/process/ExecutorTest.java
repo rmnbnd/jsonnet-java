9f38bf468f810c3a3089ce2596185bd3f966cef9
Roman Bondarchuk <rmnbnd@gmail.com>
2016-03-27 18:36:46 +0300

    [B] - Fix compile error

--- src/test/java/jsonnet/process/ExecutorTest.java
+++ src/test/java/jsonnet/process/ExecutorTest.java
@@ -1,8 +1,14 @@
 package jsonnet.process;
 
+import jsonnet.core.model.ast.AST;
+import jsonnet.core.model.ast.DesugaredObject;
+import jsonnet.core.model.ast.Local;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertNull;
 
@@ -13,8 +19,14 @@ public class ExecutorTest {
 
     @Test
     public void shouldReturnNull() {
+        // given
+        List<DesugaredObject.Field> fields = new ArrayList<>();
+        List<AST> asts = new ArrayList<>();
+        DesugaredObject object = new DesugaredObject(fields, asts);
+        Local ast = new Local(object);
+
         // when
-        String json = executor.jsonnetExecute();
+        String json = executor.jsonnetExecute(ast);
 
         // then
         assertNull(json);
